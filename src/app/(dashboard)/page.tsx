d6d814438ba770d42a5c8a9b731d56fffedd1436
theblessingokonkwo <theblessingokonkwo@gmail.com>
2024-04-11 23:32:41 +0100

    remove unused imports

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 import RevenueTemplate from "@/pattern/revenue/templates/revenue-template";
-import TransactionsTop from "@/pattern/revenue/organisms/transactions-top";
-import DateInput from "@/pattern/common/molecules/inputs/date-input";
-import { Calendar } from "@/components/ui/calendarr";
 import { useState } from "react";
-import { useGetWalletQuery } from "@/redux/services/get-wallet.api-slice";
-import { useGetUserQuery } from "@/redux/services/get-user.api-slice";
-import { useGetTransactionsQuery } from "@/redux/services/get-transactions.api-slice";
-import axios from "axios";
 
 export default function Home() {
   const [date, setDate] = useState<Date | undefined>(new Date());
